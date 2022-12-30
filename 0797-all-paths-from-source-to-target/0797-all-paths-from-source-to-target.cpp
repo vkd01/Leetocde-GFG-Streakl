9f853c81260f921a612ff4afdb9b8304b06ecc0b
Vimal Kumar Dubey <73664829+vkd01@users.noreply.github.com>
2022-12-30 19:41:29 +0530

    Time: 13 ms (90.04%), Space: 10.7 MB (67.21%) - LeetHub

--- 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.cpp
+++ 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.cpp
@@ -1,38 +1,26 @@
 class Solution {
-public:
     
-    void dfs(int vertex,vector<vector<int>> &graph, vector<int>&paths,vector<vector<int>>&ans,vector<bool> &visited,int &n){
-        
-         
-        if(visited[vertex]) return;
-        
+private:
+    void dfs(int vertex, vector<vector<int>> &graph,vector<int>&paths,vector<bool>&visited,vector<vector<int>>&ans,int &n){
+        if(visited[vertex]==true) return;
         visited[vertex] = true;
-        
         paths.push_back(vertex);
-         if(vertex==n-1) ans.push_back(paths);
-        for(auto&child:graph[vertex]){
-            
-            if(!visited[child])
-            dfs(child,graph,paths,ans,visited,n);
-            //Bcktracking !!
-        
-        }
-            paths.pop_back();
-            visited[vertex] = false;
-        
-        
+        if(vertex==n-1) ans.push_back(paths);
+        for(auto&child:graph[vertex]) dfs(child,graph,paths,visited,ans,n);
+        //Backtrack !!
+        paths.pop_back();
+        visited[vertex] = false;
     }
+public:
     vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& graph) {
         vector<vector<int>> ans;
         
-        vector<int>paths;
         int n = graph.size();
+        
         vector<bool> visited(n,false);
+        vector<int>paths;
         
-        
-        
-        dfs(0,graph,paths,ans,visited,n);
-        
+        dfs(0,graph,paths,visited,ans,n);
         
         
         return ans;
