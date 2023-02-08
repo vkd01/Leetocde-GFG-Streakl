e7173ec93979573f21e176167e0d2843b543ac37
Vimal Kumar Dubey <73664829+vkd01@users.noreply.github.com>
2023-02-08 19:20:49 +0530

    Time: 566 ms (16.88%), Space: 18.1 MB (6.85%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.cpp
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -1,26 +1,33 @@
 class Solution {
 private:
-    void dfs(vector<int> &nums, int i, int j, int n , int m){
-       
+    int dfs(vector<int>&nums,vector<int> &dp, int n, int ind){
         
+        if(ind>=n-1) return 0;
+         int ans = 1e8;
+        if(dp[ind]!=-1) return dp[ind];
+        for(int j = 1;j<=nums[ind];j++){
+             int jump;
+            if(ind+j<n){
+                jump = 1 + dfs(nums,dp,n,ind+j);
+                ans= min(ans,jump);
+            }
+            
+            
+        }
+     //   cout<<ans<<endl;
         
+        return dp[ind] = ans;
         
     }
 public:
     int jump(vector<int>& nums) {
         int n = nums.size();
-        int mx = 0, ans = 0, curr = 0;
+        int ans = 0;
         
-        for(int i = 0;i<n-1;i++){
-            mx = max(mx,i+nums[i]);
-            if(i == curr){
-                ans++;
-                curr = mx;
-            }
-        }
+        vector<int> dp(n+1, -1);
+        ans = dfs(nums,dp,n,0);
         
         
-        
-        return ans ;
+        return ans;
     }
 };
