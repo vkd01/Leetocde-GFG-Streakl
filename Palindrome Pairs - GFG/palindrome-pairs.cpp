500e9470e706a61f5c72b3c65f368f032b6ea338
Vimal Kumar Dubey <73664829+vkd01@users.noreply.github.com>
2023-04-01 22:29:29 +0530

    Added solution - LeetHub

---
+++ Palindrome Pairs - GFG/palindrome-pairs.cpp
@@ -0,0 +1,67 @@
+//{ Driver Code Starts
+//Initial template for C++
+
+#include<bits/stdc++.h> 
+using namespace std; 
+
+// } Driver Code Ends
+//User function Template for C++
+class Solution{   
+  public:
+    // Function to check if a palindrome pair exists
+    bool isPalindrome(string &s){
+        int i=0;
+        int j =s.size()-1;
+        while(i<j){
+            if(s[i++]!=s[j--])
+            return false;
+        }
+        return true;
+    }
+    bool palindromepair(int n, string arr[]) {
+        // code here
+        unordered_map<string,int>mp;
+        
+        for(int i=0; i<n;i++){
+            auto s = arr[i];
+            reverse(s.begin(),s.end());
+            mp[s] =i;
+        }
+        
+        for(int i=0; i<n;i++){
+            for(int j =0; j<arr[i].size();j++){
+                string s1 = arr[i].substr(0,j);
+                string s2 = arr[i].substr(j);
+                
+                if(mp.count(s1) && isPalindrome(s2) && mp[s1]!=i)
+                    return true;
+                if(mp.count(s2) && isPalindrome(s1) && mp[s2]!=i)
+                    return true;
+                
+            }
+        }
+        return false;
+    }
+};
+
+//{ Driver Code Starts.
+int main() 
+{ 
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int N;
+        cin >> N;
+        string arr[N];
+        for(int i = 0; i < N; i++){
+            cin >> arr[i];
+        }
+
+        Solution ob;
+        cout << ob.palindromepair(N, arr) << endl;
+    }
+    return 0; 
+} 
+
+// } Driver Code Ends
