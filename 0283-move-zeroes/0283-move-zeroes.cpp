cc38b195327df0c6e7e8a7182eef212621c4614d
Vimal Kumar Dubey <73664829+vkd01@users.noreply.github.com>
2023-04-09 04:39:35 +0530

    Time: 12 ms (98.96%), Space: 19.4 MB (17.37%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,39 +1,21 @@
 class Solution {
 public:
-    void moveZeroes(vector<int>& nums) {
+    void moveZeroes(vector<int>& arr) {
+        int left = 0, right = 0, n = arr.size();
         
-        int zero = 0;
-      
-        for(auto&i:nums) if(i==0) zero++;
-          if(zero==0) return;
-        int n = nums.size();
-        if(n==1) {
-            if(nums[0]==0) return;
-            else  return;
+        if(n==1) return;
+        
+        while(left<n and right<n){
             
+           
+            while(left<n and arr[left] != 0) left++;
+            while(right < n and (right<left or  arr[right] == 0)) right++;
+            // cout<<left<<" "<<right<<endl;
+            
+            if(left< n and right<n and right>left)
+            swap(arr[left],arr[right]);
         }
-        if(n==2){
-            if(nums[0]==0){
-                swap(nums[0],nums[1]);
-                return;
-            }
-            else return;
-        }
-        int ptr1 = 0,ptr2 = 0;
         
-        while(ptr2<n){
-            while(nums[ptr1]!=0) ptr1++;
-            if(nums[ptr2]==0) {
-                while(nums[ptr2]==0) {
-                    ptr2++;
-                    if(ptr2>=n-1) break;
-                }
-                if(ptr2<=n-1)
-                swap(nums[ptr1],nums[ptr2]);
-                //ptr1++;
-            }
-            else ptr2++;
-        }
-        for(int i=n-zero;i<n;i++) nums[i] = 0;
+        
     }
 };
