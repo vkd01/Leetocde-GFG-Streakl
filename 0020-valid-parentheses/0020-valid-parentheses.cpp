5342330ed16a8fdcf082d780dbb8d684a0199617
Vimal Kumar Dubey <73664829+vkd01@users.noreply.github.com>
2023-04-10 18:06:32 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (12.09%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+        
+        
+        for(auto&i:s){
+            
+            if(st.empty()){
+                st.push(i);
+                continue;
+            }
+            
+            
+            if(i=='(' or i=='[' or i=='{') st.push(i);
+            else if(i==')'){
+                if(st.top()=='(') st.pop();
+                else st.push(i);
+            }
+            else if(i==']'){
+                if(st.top()=='[') st.pop();
+                else st.push(i);
+            }
+            else if(i=='}'){
+                if(st.top()=='{') st.pop();
+                else st.push(i);
+            }
+        }
+        
+        
+        return st.size()==0;
+    }
+};
