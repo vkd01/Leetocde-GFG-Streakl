1b6346f56a6568472bed703ac2b4c4b34d4bd7db
Vimal Kumar Dubey <73664829+vkd01@users.noreply.github.com>
2023-06-04 18:00:28 +0530

    Time: 23 ms (83.66%), Space: 13.6 MB (98.63%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,49 +1,78 @@
 class Solution {
-private:
-    void dfs(int vertex,vector<int> *graph, vector<bool> &vis){
-          vis[vertex] = true;
-        
-        for(auto&child:graph[ vertex]){
-            if(!vis[child]) dfs(child,graph, vis);
-        }
-        
-    }
+    
+ class DSU {
+ vector<int> rank, parent;
+ public:
+ void DisjointSet(int n) {
+ rank.resize(n + 1, 0LL);
+ parent.resize(n + 1);
+ for (int i = 0; i < n; i++) parent[i] = i;
+ }
+ int find_ultimate_parent(int node) {
+ if (node == parent[node]) return node;
+ return parent[node] = find_ultimate_parent(parent[node]);
+ }
+ void Union(int u, int v) {
+ u = find_ultimate_parent(u);
+ v = find_ultimate_parent(v);
+ if (u == v) return;
+ parent[v] = u;
+ }
+ void UnionByRank(int u, int v) {
+ int ulp_u = find_ultimate_parent(u);
+ int ulp_v = find_ultimate_parent(v);
+ if (ulp_u == ulp_v) return;
+ if (rank[ulp_u] < rank[ulp_v]) parent[ulp_v] = ulp_u;
+ else if (rank[ulp_u] > rank[ulp_v]) parent[ulp_v] = ulp_u;
+ else {
+ parent[ulp_v] = ulp_u;
+ rank[ulp_u]++;
+ }}};
 public:
     int findCircleNum(vector<vector<int>>& isConnected) {
-        int ans = 0;
+        DSU dsu;
         
         int n = isConnected.size();
         
-        vector<int> graph[n+1];
+        dsu.DisjointSet(n);
         
-        vector<bool> vis(n+1, false);
+        int ans = 0;
         
         for(int i = 0;i<n;i++){
             for(int j = 0;j<n;j++){
                 if(isConnected[i][j] == 1){
-                    graph[i+1].push_back(j+1);
-                    graph[j+1].push_back(i+1);
+                    dsu.Union(i,j);
                 }
-            }                                         
-        }
-        
-        
-        for(int i = 1;i<n+1;i++){
-            if(!vis[i]) {
-                dfs(i,graph,vis);
-                ans++;
             }
         }
-     
         
-//         for(int i =0 ;i<n+1;i++){
-//             cout<<"Parent -> "<<i<<" ->";
-//             for(auto&ii:graph[i]) cout<<ii<<" ";
-//             cout<<endl;
-//         }
-        
+        for(int i = 0;i<n;i++){
+            if(dsu.find_ultimate_parent(i) == i) ans++;
+        }
         
         
         return ans;
     }
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
