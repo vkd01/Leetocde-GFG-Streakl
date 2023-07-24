a0e67386e86598b3b63dda91066ded62325c3d33
Vimal Kumar Dubey <73664829+vkd01@users.noreply.github.com>
2023-07-24 15:10:01 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (16.21%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -1,15 +1,9 @@
 class Solution {
 public:
-    
-    //RECURSIVE APPROACH
     double myPow(double x, int n) {
-        if(n == 0) return 1;
-        if(x == 0) return 0;
+          double ans = 0.00;
+        ans = pow(x,n);
         
-        if(n < 0){
-            return 1 / x * myPow(1 / x , -(n + 1));
-        }
-        
-        return (n % 2 == 0) ? myPow(x * x , n / 2) : x * myPow(x * x , n / 2);
+        return ans;
     }
 };
