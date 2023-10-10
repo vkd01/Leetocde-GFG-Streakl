44a7a33a774186b14f5836281df5e0551aab83b8
Vimal Kumar Dubey <73664829+vkd01@users.noreply.github.com>
2023-10-10 14:50:10 +0530

    Time: 383 ms (17.99%), Space: 110.1 MB (5.23%) - LeetHub

--- 2009-minimum-number-of-operations-to-make-array-continuous/2009-minimum-number-of-operations-to-make-array-continuous.cpp
+++ 2009-minimum-number-of-operations-to-make-array-continuous/2009-minimum-number-of-operations-to-make-array-continuous.cpp
@@ -1,44 +1,28 @@
 class Solution {
 public:
-    int minOperations(vector<int>& arr) {
-        int ans = 1e9 + 10;
-        
-        int n = arr.size();
-        
-        map<int,int> mp;
-        
-        for(auto&i:arr) mp[i]++;
-
+    int minOperations(vector<int>& nums) {
+        set<int> st;
         vector<int> v;
-  
-            for(auto&i:mp){
-                if(i.second>0){
-                    v.push_back(i.first);
-                    i.second--;
-                }
-            }       
-   
-     
+        int n = nums.size();
+        for(auto it : nums) st.insert(it);
         
-        for(int i = 0;i<v.size();i++){
+        
+        for(auto x : st) v.push_back(x);
+        
+        int ans = 1e9;
+        
+        for(int i=0;i<v.size();i++){
+            int left=v[i];
+            int right = n+left-1;
             
-            int left = v[i];
-            
-            int right = n + left -1;
-            
-            auto it = upper_bound(v.begin(),v.end(), right);
-            int remove = it - v.begin();
-            int temp = n - (remove  - i);
-            
-            ans = min(ans, temp);
- 
-            
+            auto it = upper_bound(v.begin(),v.end(),right);
+            int remove = (it-v.begin())-i;
+            ans=min(ans,n-remove);
         }
-        
-        
-        
-        
-        
         return ans;
+        
+        
+            
+            
     }
 };
