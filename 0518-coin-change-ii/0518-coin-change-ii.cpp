77965b04c57fd6efdbb0ff223acd27972b1fa4f0
Vimal Kumar Dubey <73664829+vkd01@users.noreply.github.com>
2023-12-02 13:33:04 +0530

    Time: 28 ms (43.05%), Space: 21.9 MB (12.28%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -1,25 +1,36 @@
 class Solution {
+private:
+    int dfs(int i, int curr,int sum, vector<int>&arr, vector<vector<int>>&dp){
+        int n = arr.size();
+        if(i>=n) return 0;
+        
+        if(curr==sum) return 1;
+        
+        if(dp[i][curr] != -1) return dp[i][curr];
+        
+        
+        int ans = 0;
+        
+        if(arr[i] + curr<=sum){
+           ans = ans + dfs(i, curr+arr[i],sum, arr,dp);
+        } 
+        
+        ans = ans + dfs(i+1, curr,sum, arr,dp);
+        
+        
+        return dp[i][curr] = ans;
+        
+    }
 public:
-int dp[5001][301];
-int solve(int amount,vector<int>&coins,int i){
-    if(amount==0){
-        return 1;
-    }
-    if(dp[amount][i]!=-1)
-    return dp[amount][i];
-
-    if(i==coins.size())
-    return 0;
-    
-    int tk=0;
-    if(coins[i]<=amount){
-        tk=solve(amount-coins[i],coins,i);
-    }
-    int nt=solve(amount,coins,i+1);
-    return dp[amount][i]=tk+nt;
-}
     int change(int amount, vector<int>& coins) {
-        memset(dp,-1,sizeof(dp));
-         return solve(amount,coins,0);
+        int ans = 0, n = coins.size();
+        
+        vector<vector<int>>dp(n+1, vector<int>(amount+100,-1));
+        
+        ans = dfs(0,0,amount,coins,dp);
+        
+        
+        
+        return ans;
     }
 };
