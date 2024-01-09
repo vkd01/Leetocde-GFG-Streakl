20c6c13fb7f357f22367825d30ab74a48ba2ea49
Vimal Kumar Dubey <73664829+vkd01@users.noreply.github.com>
2024-01-09 10:54:51 +0530

    Time: 0 ms (100.00%), Space: 6.7 MB (8.41%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,26 +1,29 @@
 class Solution {
-    int f(int n, int &ans, vector<int>&dp){
-        if(n==0) return 1;
-        else if(n<0) return 0;
-        
-        if(dp[n]!=-1) return dp[n];
-        
-        ans+= f(n-1,ans,dp);
-        dp[n] = ans;
-        ans+= f(n-2,ans,dp);
-        dp[n] = ans;
-        
-        return 0;
-    }
+
+int rec(int step, int n, vector<int>&dp){
+    
+    //base case
+    if(step>n) return 0;
+    if(step==n) return 1;
+    
+    if(dp[step] != -1) return dp[step];
+    
+    
+    int one_step = rec(step+1, n, dp);
+    int two_step = rec(step+2, n,dp);
+    
+    return dp[step] = one_step + two_step;
+    
+}
+    
 public:
     int climbStairs(int n) {
+        
         vector<int> dp(n+1,-1);
         
-        int ans = 0;
+        int ans = rec(0,n, dp);
         
-        int fuck = f(n,ans,dp);
         
         return ans;
-        
     }
 };
