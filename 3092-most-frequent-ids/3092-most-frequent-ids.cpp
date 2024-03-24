0989d3216cc8d9ad1b6b953c23800fc16ac05e48
Vimal Kumar Dubey <73664829+vkd01@users.noreply.github.com>
2024-03-24 15:18:13 +0530

    Time: 487 ms (20.00%), Space: 312.7 MB (20.00%) - LeetHub

--- 3092-most-frequent-ids/3092-most-frequent-ids.cpp
+++ 3092-most-frequent-ids/3092-most-frequent-ids.cpp
@@ -1,52 +1,84 @@
 typedef long long ll;
-const ll N = 4e5 + 10;
+const ll N = 2e5 + 10;
+  /*----------------------------SEGMENT TREE-------------------------------------------------*/
+ class SegmentTree {
 
-class SegmentTree{
+ public:
+  vector<ll> segTree;
 
-    public :
-    vector<ll> tree;
-    SegmentTree(){
-        tree.resize(4*N,0);
-    }
+  SegmentTree(){
+    segTree.resize(4*N);
+  }
+ 
+ ll combine(ll a, ll b){
+    return max(a,b);
+ }
 
-    void update(ll node, ll left, ll right, ll ind, ll val){
-        if(left == right){
-            tree[node]+=val;
-            return;
-        }
+public:
 
-        ll mid = left + (right - left)/2;
+ 
+ void update(ll node, ll start, ll end, ll idx, ll val) {
+   if(start == end){
+ // Leaf node
+     segTree[node] += val;
+   }
+   else{
+     ll mid = start + (end - start) / 2;
+     if(start <= idx and idx <= mid){
+       update(2*node+1, start, mid, idx, val);
+     }
+     else {
+       update(2*node+2, mid+1, end, idx, val);
+     }
+     segTree[node] = combine(segTree[2*node+1] ,segTree[2*node+2]);
+   }
+ }
 
-        if(ind<=mid) update(2*node+1, left, mid, ind, val);
-        else update(2*node+2, mid+1, right, ind, val);
+ ll query(ll node, ll start, ll end, ll left, ll right){
+   if(right < start or end < left)  return 0;
+   if(left <= start and end <= right) return segTree[node];
 
-        tree[node] = max(tree[2*node+1], tree[2*node+2]);
-
-    }
-
+   ll mid = start + (end - start)/2;
+   ll p1 = query(2*node+1, start, mid, left, right);
+   ll p2 = query(2*node+2, mid+1, end, left, right);
+   return combine(p1 , p2);
+ }
 };
-
 
 
 class Solution {
 public:
     vector<long long> mostFrequentIDs(vector<int>& arr, vector<int>& freq) {
-
         vector<ll> ans;
-
-        SegmentTree segtree;
-        
         ll n = arr.size();
         
-        for(ll i = 0;i<n;i++){
-
-            ll ele = arr[i], times = freq[i];
-            segtree.update(0,0,N-1, ele, times);
-            ans.push_back(segtree.tree[0]);
+        SegmentTree segtree;
+        
+        for(int i = 0;i<n;i++){
+            ll ind = arr[i], val = freq[i];
+            
+            segtree.update(0,0,N-1, ind, val);
+            
+            ans.push_back(segtree.segTree[0]);
         }
-
-
-
+        
+        
         return ans;
     }
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
